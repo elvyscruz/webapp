ce5ccbc68ce33404023909b3f361d87257c07760
elvys <elvys.cruz@gmail.com>
2025-08-05 15:20:15 -0400

    ci: se agrego deploy en render

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3008;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo Express desde el IITLA!');
+  res.send('¡Hola Mundo Express desde el IITLA desplegado en RENDER!');
 	});
 
 	app.listen(port, () => {
