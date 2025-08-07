7474e4c0fc4be6b2aae14a68d811e7d6c937111c
elvys <elvys.cruz@gmail.com>
2025-08-07 19:51:40 -0400

    refactor: mensaje modificado

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3008;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo Devops desde el ITLA!');
+  res.send('¡Hola Mundo Devops desde el ITLA!, ahora con IA');
 	});
 
 	app.listen(port, () => {
