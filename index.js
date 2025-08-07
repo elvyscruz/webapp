6f85a0f3c69c5faa16eb61ec6b3b4f255968e4ad
elvys <elvys.cruz@gmail.com>
2025-08-07 19:40:25 -0400

    fix typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3008;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo Devpops desde el ITLA  con RENDER!');
+  res.send('¡Hola Mundo Devops desde el ITLA!');
 	});
 
 	app.listen(port, () => {
